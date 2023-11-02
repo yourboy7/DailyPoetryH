1608c114bbcd026409f8584b32a790de5e35c3b5
yourboy77 <243864016@qq.com>
2023-11-03 05:36:35 +0800

    1

--- 源码/DailyPoetryH/DailyPoetryH.UnitTest/Services/PoetryStorageTest.cs
+++ 源码/DailyPoetryH/DailyPoetryH.UnitTest/Services/PoetryStorageTest.cs
@@ -14,4 +14,16 @@ public class PoetryStorageTest {
         var poetryStorage = new PoetryStorage(mockPreferenceStorage);
         Assert.True(poetryStorage.IsInitialized);
     }
+
+    [Fact]
+    public void IsInitialized_NotInitialized()
+    {
+        var preferenceStorageMock = new Mock<IPreferenceStorage>();
+        preferenceStorageMock
+            .Setup(p => p.Get(PoetryStorageConstant.DbVersionKey, 0))
+            .Returns(0);
+        var mockPreferenceStorage = preferenceStorageMock.Object;
+        var poetryStorage = new PoetryStorage(mockPreferenceStorage);
+        Assert.False(poetryStorage.IsInitialized);
+    }
 }
